aa16a1acc63e5ab389aeebb7eca9922fd28386e6
Jurasio54 <103755947+Jurasio54@users.noreply.github.com>
2022-06-14 10:45:15 -0400

    Add files via upload

---
+++ start.py
@@ -0,0 +1,6 @@
+def start_i(username):
+    msg = '🎩Bot: Moodle7-4 7.4\n'
+    msg+= '🌆Bienvenido @'+ str(username)+'\n'
+    msg+= '🛠️Desarrollador: @diago8888\n'   
+    msg+= '📝Guía: Manda el comando /help para obtener una ayuda\n'   
+    return msg
